ef39bcccdbbbca535ca44b8464a46f1c787e9145
kiziri <58429073+kiziri@users.noreply.github.com>
2022-01-20 04:54:27 +0900

    Example code practice about Reflection API
    스프링 프레임워크를 통해 외부로부터 private 메서드 호출되는 구조 학습
    Reflection API의 구조 학습을 통한 외부 호출의 차이 확인

---
+++ src/main/java/com/fastcampus/ch2/Main.java
@@ -0,0 +1,21 @@
+package com.fastcampus.ch2;
+
+import java.lang.reflect.Method;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+/*        Hello hello = new Hello();
+        hello.main3();*/  // private로 인한 외부 호출 불가
+
+        // Spring Framework에서 많이 쓰이고 있는 API에 대해 알아봄
+        // Reflection API를 사용 - 클래스 정보를 얻고 다룰 수 있는 강력한 기능제공
+        // java.lang.reflection 패키지를 제공
+        Class helloClass = Class.forName("com.fastcampus.ch2.Hello");
+        Hello hello = (Hello) helloClass.newInstance(); // Class 객체가 가진 정보로 객체를 생성
+        Method main = helloClass.getDeclaredMethod("main3");
+        main.setAccessible(true);
+        
+        main.invoke(hello); // hello.main(); 과 동일함
+    }
+}
