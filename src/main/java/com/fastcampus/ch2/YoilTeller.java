4d4b8bee79b384a1d18a459dd76dd804795d49a9
kiziri <58429073+kiziri@users.noreply.github.com>
2022-01-28 22:32:22 +0900

    날짜를 입력받아 요일을 출력받는 서비스 연습 1

--- src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -1,30 +1,31 @@
 package com.fastcampus.ch2;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Calendar;
 
+
+@Controller
+// 년월일을 입력하면, 요일을 알려주는 프로그램
 public class YoilTeller {
     // 1. 일반 static 메서드의 통신
     public static void main(String[] args) {
-        // 1. 입력
+        // 1. 입력 : 매개변수로 스트링 문자열로 받아 저장
         // 일반적인 날짜와 요일을 위한 변수 선언
         String year = args[0];
         String month = args[1];
         String day = args[2];
 
+        // 문자열을 정수(숫자)로 변환
         int yyyy = Integer.parseInt(year);
         int mm = Integer.parseInt(month);
         int dd = Integer.parseInt(day);
 
-        // 2. 작업
+        // 2. 작업 : 날짜 세팅
         Calendar cal = Calendar.getInstance();
         cal.set(yyyy, mm-1, dd);
 
+        // 요일을 알아내는 부문 ex. 1:일요일, 2:월요일...
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
         char yoil = " 일월화수목금토".charAt(dayOfWeek);
 
@@ -34,7 +35,7 @@ public class YoilTeller {
         System.out.println(yoil + "요일입니다.");
     }
 
-    // 2. 외부로부터의 통신 방식을 통한 통신 연결 메서드
+    /*// 2. 외부로부터의 통신 방식을 통한 통신 연결 메서드
     @RequestMapping("/getYoil")
     public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // 1. 입력
@@ -70,5 +71,5 @@ public class YoilTeller {
         out.println("</html>");
         out.close();
         // 출력 스트림 내용 입력 설정 부분 끝 //
-    }
+    }*/
 }
