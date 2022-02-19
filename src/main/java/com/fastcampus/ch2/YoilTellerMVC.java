7ac7d35223877df420baa2723c69cb8835bcb099
kiziri <58429073+kiziri@users.noreply.github.com>
2022-02-19 19:21:25 +0900

    요일 출력 서비스 및 View 구현

--- src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -1,55 +1,52 @@
 package com.fastcampus.ch2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Calendar;
 
 @Controller
 // 년월일을 입력하면, 요일을 알려주는 프로그램
 public class YoilTellerMVC {
     // 2. 외부로부터의 통신 방식을 통한 통신 연결 메서드
-    // http://localhost:8080/ch2/getYoil?year=2022&month=1&day=31
-    @RequestMapping("/getYoil")
-    public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // 1. 입력 : HTTP 통신으로부터 데이터를 전송받음
-        // 브라우저(외부)부터 날짜와 요일을 위한 변수의 값을 받음
-        String year = request.getParameter("year");
-        String month = request.getParameter("month");
-        String day = request.getParameter("day");
+    // http://localhost:8080/ch2/getyoilmvc?year=2022&month=1&day=31
+    @RequestMapping("/getyoilmvc")
+    public String main(int year, int month, int day, Model model) throws IOException {
 
-        // 문자열을 정수(숫자)로 변환
-        int yyyy = Integer.parseInt(year);
-        int mm = Integer.parseInt(month);
-        int dd = Integer.parseInt(day);
+        // 1. 유효성 검사
+        if (!isVaild(year, month, day)) {
+            return "yoilError";
+        }
 
-        // 2. 작업 : 날짜 세팅
+        // 2. 처리 과정 호출
+        char yoil = getYoil(year, month, day);
+
+        // 3. Model에 작업 결과 저장
+        model.addAttribute("year", year);
+        model.addAttribute("month", month);
+        model.addAttribute("day", day);
+        model.addAttribute("yoil", yoil);
+
+        // 4. 작업 결과를 보여줄 View의 이름을 반환
+        return "yoil";
+    }
+
+    private boolean isVaild(int year, int month, int day) {
+        if (year==-1 || month==-1 || day==-1) {
+            return false;
+        }
+        return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크
+    }
+
+    private char getYoil(int year, int month, int day) {
+        // 작업 : 날짜 세팅
         Calendar cal = Calendar.getInstance();
-        cal.set(yyyy, mm-1, dd);
+        cal.set(year, month - 1, day);
 
         // 요일을 알아내는 부문 ex. 1:일요일, 2:월요일...
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-        char yoil = " 일월화수목금토".charAt(dayOfWeek);
-
-        // 3. 출력
-        // 요청받은 브라우저(외부)로 요청 결과를 전송하는 출력(브라우저로 결과를 확인 가능함)
-        response.setContentType("text/html");   // 응답의 형식을 html로 지정
-        response.setCharacterEncoding("utf-8"); // 응답의 인코딩 형식을 utf-8로 지정
-        PrintWriter out = response.getWriter(); // 브라우저로 출력 스트림(out)을 얻음
-        // 출력 스트림 내용 입력 설정 부분 시작 //
-        out.println("<html>");
-        out.println("<head>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println(year + "년 " + month + "월 " + day + "일은 ");
-        out.println(yoil + "요일입니다.");
-        out.println("</body>");
-        out.println("</html>");
-        out.close();
-        // 출력 스트림 내용 입력 설정 부분 끝 //
+        return " 일월화수목금토".charAt(dayOfWeek);
     }
 }
--- src/main/webapp/WEB-INF/views/yoil.jsp
+++ src/main/webapp/WEB-INF/views/yoil.jsp
@@ -1,3 +1,4 @@
+<%@ page contentType="text/html; charset=utf-8" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <html>
@@ -5,6 +6,6 @@
 	<title>YoilTellerMVC</title>
 </head>
 <body>
-	<P> ${year }년 ${month }월 ${day }일은 ${yoil }입니다.</P>
+	<P> ${year }년 ${month }월 ${day }일은 ${yoil }요일 입니다.</P>
 </body>
 </html>
