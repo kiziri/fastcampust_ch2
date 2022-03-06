48f2c1cc9c83b6c647927f3afe6605d45f721da2
kiziri <58429073+kiziri@users.noreply.github.com>
2022-03-06 15:43:24 +0900

    쿠키 기본 개념 관련 실습 - 메인 홈 화면

--- src/main/java/com/fastcampus/ch2/HomeController.java
+++ src/main/java/com/fastcampus/ch2/HomeController.java
@@ -1,15 +1,13 @@
 package com.fastcampus.ch2;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Locale;
 
 /**
  * Handles requests for the application home page.
@@ -25,16 +23,7 @@ public class HomeController {
 	// 2. URL과 메서드 연결(맵핑, mapping) 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return "index";
 	}
 	
 }
