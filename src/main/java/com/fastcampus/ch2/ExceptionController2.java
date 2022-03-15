da336becaba2cdbf1a86aa022d9eb77114b290b5
kiziri <58429073+kiziri@users.noreply.github.com>
2022-03-16 03:15:22 +0900

    예외처리 관련 기본 개념 실습 - 사용자 정의 에러코드 예외처리 적용

--- src/main/java/com/fastcampus/ch2/ExceptionController2.java
+++ src/main/java/com/fastcampus/ch2/ExceptionController2.java
@@ -1,30 +1,24 @@
 package com.fastcampus.ch2;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.io.FileNotFoundException;
+@ResponseStatus(HttpStatus.BAD_REQUEST) // 500 -> 400
+class MyException extends RuntimeException {
+    MyException(String message) {
+        super(message);
+    }
+    MyException() { this(""); }
+}
 
 @Controller
 public class ExceptionController2 {
-    @ExceptionHandler({NullPointerException.class, FileNotFoundException.class})
-    public String catcher2(Exception ex, Model m) {
-        m.addAttribute("ex", ex);
-        return "error";
-    }
-
-    @ExceptionHandler(Exception.class)
-    public String catcher(Exception ex, Model m) {
-        m.addAttribute("ex", ex);
-        return "error";
-    }
 
     @RequestMapping("/ex3")
-    public String main(Model m) throws Exception {
-        m.addAttribute("msg", "message from ExceptionController.main()");
-        throw new Exception("예외가 발생했습니다.");
+    public String main() throws Exception {
+        throw new MyException("예외가 발생했습니다.");
     }
 
     @RequestMapping("/ex4")
